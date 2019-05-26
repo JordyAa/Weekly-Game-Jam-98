1c830c05caf70ab8482b31e2b2ad868cd7796ca9
Jordy Aaldering <aalderingjordy@gmail.com>
2019-05-26 22:05:09 +0200

    enemy movement fix

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -5,8 +5,8 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private float fovRadius = 5f;
     [SerializeField] private float fovDistance = 10f;
 
-    private Dragon player;
     private Transform target;
+    private Dragon player;
 
     private Head head;
     private CombatController combat;
@@ -14,8 +14,8 @@ public class EnemyController : MonoBehaviour
     private void Start()
     {
         GameObject p = GameObject.FindGameObjectWithTag("Player");
-        player = p.GetComponentInChildren<Dragon>();
-        target = p.GetComponentInChildren<Head>().transform;
+        target = p.transform.GetChild(0);
+        player = p.GetComponent<Dragon>();
         player.OnDeath += Deregister;
         
         head = GetComponentInChildren<Head>();
@@ -24,21 +24,24 @@ public class EnemyController : MonoBehaviour
 
     private void Update()
     {
-        head.rotate = 0f;
-        
-        if (player.isDead || SceneController.isPaused) return;
-        
-        Vector3 facing = head.transform.up;
-        Vector3 dir = target.position - transform.position;
+        if (player.isDead || SceneController.isPaused)
+        {
+            head.rotate = 0f;
+            return;
+        }
+
+        Transform t = head.transform;
+        Vector3 facing = t.up;
+        Vector3 dir = target.position - t.position;
         
         float angle = Mathf.Atan2(
                           Vector3.Dot(Vector3.forward, Vector3.Cross(dir, facing)),
                           Vector3.Dot(dir, facing)) *
                       Mathf.Rad2Deg;
         
-        if (Mathf.Abs(angle) < fovRadius && Vector2.Distance(head.transform.position, target.position) < fovDistance)
+        if (Mathf.Abs(angle) < fovRadius && Vector2.Distance(t.position, target.position) < fovDistance)
         {
-            combat.ShootFireball(head.transform);
+            combat.ShootFireball(t);
         }
 
         head.rotate = angle > 0 ? 1f : -1f;
@@ -46,6 +49,6 @@ public class EnemyController : MonoBehaviour
 
     private static void Deregister(Dragon dragon)
     {
-        GameObject.FindObjectOfType<SpawnController>().spawned -= 1;
+        FindObjectOfType<SpawnController>().spawned -= 1;
     }
 }
