2dd677d0a91e56841601f77549347a9228f22c9a
Jordy Aaldering <aalderingjordy@gmail.com>
2019-05-26 14:45:14 +0200

    added enemy movement and shooting

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private float fovRadius = 1f;
+    
+    private Transform target;
+
+    private Head head;
+    private CombatController combat;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Head>().transform;
+        
+        head = GetComponentInChildren<Head>();
+        combat = GetComponent<CombatController>();
+    }
+
+    private void Update()
+    {
+        Vector3 facing = head.transform.up;
+        Vector3 dir = target.position - transform.position;
+        
+        float angle = Mathf.Atan2(
+                          Vector3.Dot(Vector3.forward, Vector3.Cross(dir, facing)),
+                          Vector3.Dot(dir, facing)) *
+                      Mathf.Rad2Deg;
+        
+        if (Mathf.Abs(angle) < fovRadius)
+        {
+            combat.ShootFireball(head.transform);
+        }
+
+        head.rotate = angle > 0 ? 1f : -1f;
+    }
+}
