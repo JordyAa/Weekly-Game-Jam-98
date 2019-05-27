58e0b772f9b19ad5ed6eac4a6f4be10be534f708
Jordy Aaldering <aalderingjordy@gmail.com>
2019-05-27 21:25:48 +0200

    high score main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,8 +1,21 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI highScoreText = null;
+
+    private void Start()
+    {
+        int highScore = PlayerPrefs.GetInt("highScore");
+        if (highScore > 0)
+        {
+            highScoreText.text = "HIGH SCORE: " + highScore;
+            highScoreText.enabled = true;
+        }
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
